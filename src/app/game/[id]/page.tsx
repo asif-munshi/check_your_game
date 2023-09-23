238b5b222eade47ad745d13652a11f235e8c51cf
asif-munshi <asifmunshe2978@gmail.com>
2023-09-24 01:28:00 +0600

    updated_page

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,74 +1,35 @@
 "use client";
 
-import { useRouter, useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { getGameById } from "@/lib/game";
+import { Game } from "@/types/game";
+import BounceLoading from "@/components/Loading/BounceLoading";
 
 export default function Game() {
   const params = useParams();
   const { id } = params;
   const paramsID = id;
 
+  const { data, isLoading } = useQuery<Game[]>({
+    queryKey: ["game"],
+    queryFn: () => getGameById(paramsID),
+  });
+
+  if (isLoading || !data) return <BounceLoading />;
+
+  const createDate = new Date(data.createdAt);
+  const updateDate = new Date(data.updatedAt);
+
   return (
-    <div className="flex h-screen w-full items-center justify-center text-white">
-      <div>Game Page Number : {paramsID}</div>
-    </div>
+    <main className="flex h-full flex-col items-center justify-start gap-8 p-12 text-white">
+      <div className="flex flex-col rounded-xl border border-blue-400 p-4 shadow-lg">
+        <div>id: {data.id}</div>
+        <div>title: {data.title}</div>
+        <div>createdAt: {createDate.toLocaleDateString()}</div>
+        <div>createdAt: {updateDate.toLocaleDateString()}</div>
+      </div>
+    </main>
   );
 }
-// "use client";
-
-// import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-// import Link from "next/link";
-// import { useRouter, useParams } from "next/navigation";
-// import { getTodoById, deleteTodo, TodoList } from "@/lib/game";
-// import BounceLoading from "@/components/Loading/BounceLoading";
-
-// export default function Game() {
-//   const router = useRouter();
-//   const params = useParams();
-//   const { id } = params;
-//   const paramsID = id;
-
-//   const queryClient = useQueryClient();
-
-//   const { data, isLoading } = useQuery<TodoList[]>({
-//     queryKey: ["todo"],
-//     queryFn: () => getTodoById(paramsID),
-//   });
-
-//   const { mutate } = useMutation({
-//     mutationFn: (id) => deleteTodo(id),
-//     onSuccess: () => {
-//       queryClient.invalidateQueries(["todo"]), router.push("/dashboard");
-//     },
-//   });
-
-//   if (isLoading || !data) return <main>Loading...</main>;
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-12">
-//       <div
-//         className="flex flex-col rounded-xl border border-blue-400 p-4 shadow-lg"
-//         key={data.id}
-//       >
-//         <div className="text-lg font-bold">{data.title}</div>
-//         <div>{data.description}</div>
-//       </div>
-//       <div className="flex flex-row justify-between">
-//         <Link
-//           href={`/updatetodo/${data.id}`}
-//           type="button"
-//           className="rounded-full border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white"
-//         >
-//           Update
-//         </Link>
-//         <button
-//           disabled={isLoading}
-//           onClick={() => mutate(data.id)}
-//           type="button"
-//           className="rounded-full border border-blue-500 bg-transparent px-4 py-2 font-semibold text-blue-700 hover:border-transparent hover:bg-blue-500 hover:text-white"
-//         >
-//           Delete
-//         </button>
-//       </div>
-//     </main>
-//   );
-// }
