0b148e27e7c22b4f987f2c1ac2b325e4113078c1
asif-munshi <asifmunshe2978@gmail.com>
2023-09-23 16:34:11 +0600

    added_active_route

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,27 +1,43 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Search from "./Search";
 
 export default function Main() {
+  const pathname = usePathname();
   return (
-    <main className="sticky z-[999] flex h-[100px] w-full justify-center">
+    <main className="sticky z-[999] flex h-[100px] w-full justify-center py-4">
       <div className="w-9/12">
         <div className="flex h-full w-full flex-row items-center justify-start gap-8 text-[14px] text-[#f5f5f5]">
           <Search />
           <Link
-            href={"/"}
-            className="opacity-80 transition-colors duration-150 ease-in-out hover:opacity-100"
+            href={"/discover"}
+            className={`${
+              pathname.includes("/discover")
+                ? "opacity-100"
+                : "opacity-50 transition-colors duration-150 ease-in-out hover:opacity-100"
+            }`}
           >
             Discover
           </Link>
           <Link
-            href={"#"}
-            className="opacity-80 transition-colors duration-150 ease-in-out hover:opacity-100"
+            href={"/browse"}
+            className={`${
+              pathname.includes("/browse")
+                ? "opacity-100"
+                : "opacity-50 transition-colors duration-150 ease-in-out hover:opacity-100"
+            }`}
           >
             Browse
           </Link>
           <Link
-            href={"#"}
-            className="opacity-80 transition-colors duration-150 ease-in-out hover:opacity-100"
+            href={"/news"}
+            className={`${
+              pathname.includes("/news")
+                ? "opacity-100"
+                : "opacity-50 transition-colors duration-150 ease-in-out hover:opacity-100"
+            }`}
           >
             News
           </Link>
